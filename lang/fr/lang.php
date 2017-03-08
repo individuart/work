6207c55265530392b32d7960d2613a3c29822247
Jesús García <chugns@gmail.com>
2017-03-08 08:30:08 +0100

    backend french tranlation

---
+++ lang/fr/lang.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+  'plugin' => [
+    'description' => 'Liste des projets'
+  ],
+  'backend' => [
+    'label_published' => 'Publié',
+    'label_name' => 'Nom',
+    'label_image' => 'Image',
+    'label_description' => 'Description',
+    'label_long_description' => 'Description longue',
+    'label_content' => 'Contenu',
+    'label_work_date' => 'Date du projet',
+    'label_featured_image' => 'Image',
+    'label_order' => 'Ordre',
+    'new_work' => 'Nouveau projet',
+    'order_works' => 'Ordre des projets',
+    'work_list' => 'List des projets',
+    'work' => 'projet',
+    'works' => 'Projets',
+    'create_work' => 'Créer un projet',
+    'edit_work' => 'Modifier un projet',
+    'preview_work' => 'Visualiser un projet',
+    'manage_works' => 'Gérer les projets',
+    'reorder_works' => 'Reorganiser les projet',
+    'return_work_list' => 'Retour à la liste des projets',
+    'successfully_deleted' => 'Enregistrements supprimés avec succes',
+    'delete_confirm' => 'oulez-vous supprimer cet/s enregistrement(s) ?',
+    'delete_selected' => 'Supprimer le(s) sélectionné(s)',
+    'worklist_description' => 'Présentation des différents projets',
+    'category' => 'Catégorie',
+    'categories' => 'Catégoríes',
+    'new_category' => 'Nouvelle Categorie',
+    'create_category' => 'Créer Categorie',
+    'edit_category' => 'Modifier Categorie',
+    'preview_category' => 'Preview Categorie',
+    'manage_categories' => 'Gérer Categories',
+    'return_to_list' => 'Retour à la liste',
+    'saving' => 'Enregistrement...',
+    'deleting' => 'Suppression',
+    'delete_confirm_record' => 'Voulez-vous supprimer cet/s enregistrement(s) ?',
+    'reorder_categories' => 'Reorganiser les catégories',
+    'order_categories' => 'Organiser les catégories',
+    'category_list' => 'Liste des catégories'
+  ]
+];
+
